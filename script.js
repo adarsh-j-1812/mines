5e1d4d2a7da43b07608e675f5b97f5fb46746886
adarsh-j-1812 <155352001+adarsh-j-1812@users.noreply.github.com>
2025-02-10 23:22:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let sqs = document.getElementsByClassName("sqs");
+let ale = prompt("Enter the number of mines");
+let mines = [];
+let rand;
+
+for (let i = 0; mines.length < ale; i++) {
+  rand = Math.floor(Math.random() * (35 + 0 - 0));
+  if (!mines.includes(rand)) {
+    mines.push(rand);
+  }
+}
+
+// console.log(mines);
+
+Array.from(sqs).forEach((e, i) => {
+  e.addEventListener("click", function () {
+    if (mines.includes(i)) {
+      this.style.backgroundColor = "red";
+      for (let k = 0; k < mines.length; k++) {
+        sqs[mines[k]].style.backgroundColor = "red";
+        sqs[mines[k]].style.scale = "1.1";
+        setTimeout(() => {
+          sqs[mines[k]].style.scale = "1";
+        }, 500);
+    }
+    setTimeout(() => {
+        alert("Game Over");
+        location.reload();
+        this.style.pointerEvents = "none"; 
+    }, 1000);
+    } 
+    else {
+      this.style.backgroundColor = "green";
+    }
+  });
+});
